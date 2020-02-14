2b0d63eb832c8de5df9803636425c5425f0f5636
Rameş Aliyev <creator@ramesaliyev.com>
2020-02-14 03:29:26 +0300

    :sparkles: more basics/function

---
+++ README.md
@@ -0,0 +1,1 @@
+required python version `>=3.8.1`
--- basics/function.py
+++ basics/function.py
@@ -47,3 +47,27 @@ def kword(a='a', b='b', c='c'):
 kword()
 kword('x', 'y', 'z')
 kword(c='3', a='1', b='2')
+
+# keywords & arguments
+def magics(a, *arguments, **keywords):
+    print(a)
+    for arg in arguments:
+        print('arg', arg)
+    print("-" * 40)
+    for kw in keywords:
+        print('kw', kw, ":", keywords[kw])
+
+magics('1', '2', '3', _4='4', _5='5')
+
+# def f(pos1, pos2, /, pos_or_kwd, *, kwd1, kwd2):
+def standard_arg(arg):
+  print(arg)
+
+def pos_only_arg(arg, /):
+  print(arg)
+
+def kwd_only_arg(*, arg):
+  print(arg)
+
+def combined_example(pos_only, /, standard, *, kwd_only):
+  print(pos_only, standard, kwd_only)
